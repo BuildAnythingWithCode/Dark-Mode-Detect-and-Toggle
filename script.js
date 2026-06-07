68d2a8a8134c95c9905ea904ab8b46618a3fce8d
BuildAnythingWithCode <buildanythingwithcode@gmail.com>
2026-06-07 15:32:56 +0800

    fix it!

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.className = 'dark';
+  document.querySelector('.theme-name').textContent = 'dark';
+} else {
+  document.documentElement.className = 'light';
+  document.querySelector('.theme-name').textContent = 'light';
+}
+
 function setTheme() {
   const root = document.documentElement;
   const newTheme = root.className === 'light' ? 'dark' : 'light';
